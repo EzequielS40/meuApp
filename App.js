67a6a0cb39b4faab5b739b92b595c4f87cdca440
EzequielS40 <zequesilva40@gmail.com>
2023-10-09 03:45:26 -0900

    sonic_react

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,ImageBackground} from 'react-native';
+import sonic from './assets/sonic.gif';
+import estrada from './assets/estrada.jpg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source= {estrada}resizeMode='cover' style ={{width:'100%', height:'100%'}}>
+      <Image source={sonic} style ={styles.img}/>
+      </ImageBackground>
+      
+      {/* <Text style={styles.texto}>Sonic</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#555',     
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+img: {width:180,
+  height:150,
+  marginTop:300,
+  marginLeft:270,
+},
+
+    texto:{
+      fontSize: 55,
+      fontStyle:'italic',
+      fontWeight:'bold',
+      color:'#0000FF',
+      
+    },
 });
